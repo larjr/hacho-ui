aa533247006b6dc1cc68ab97fdc8b3f31c1bb28b
luisangelorjr <lrodrigues.resource@alelo.com.br>
2023-08-16 23:38:39 -0300

    ADD: home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,21 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-{
-  path: 'youtube',
-  loadChildren: () => import('./feature/youtube/youtube.module')
-  .then(m => m.YoutubeModule)
-},
-{
-  path: 'video-tools',
-  loadChildren: () => import('./feature/video-tools/video-tools.module')
-  .then(m => m.VideoToolsModule)
-},
-{
-  path: 'podcast-dex',
-  loadChildren: () => import('./feature/podcast-dex/podcast-dex.module')
-  .then(m => m.PodcastDexModule)
-}];
+  {
+    path: '',
+    loadChildren: () => import('./core/home-page/home-page.module')
+    .then(m => m.HomePageModule)
+  },
+  {
+    path: 'youtube',
+    loadChildren: () => import('./feature/youtube/youtube.module')
+    .then(m => m.YoutubeModule)
+  },
+  {
+    path: 'video-tools',
+    loadChildren: () => import('./feature/video-tools/video-tools.module')
+    .then(m => m.VideoToolsModule)
+  },
+  {
+    path: 'podcast-dex',
+    loadChildren: () => import('./feature/podcast-dex/podcast-dex.module')
+    .then(m => m.PodcastDexModule)
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { VideoToolsComponent } from './feature/video-tools/video-tools.component';
-import { PodcastDexComponent } from './feature/podcast-dex/podcast-dex.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
