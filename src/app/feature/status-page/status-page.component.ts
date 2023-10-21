a6d7cea377c89c1a6cbcb8166ce284172c4e2033
luisangelorjr <lrodrigues.resource@alelo.com.br>
2023-10-21 13:40:58 -0300

    DAILY

--- src/app/feature/status-page/status-page.component.ts
+++ src/app/feature/status-page/status-page.component.ts
@@ -4,16 +4,36 @@ import { Subject, combineLatest, throwError } from "rxjs";
 import { catchError, takeUntil } from "rxjs/operators";
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
 import { Status } from 'src/app/shared/responses/hacho/status.model';
+import { NgFor, NgIf } from '@angular/common';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+
+interface Alert {
+	type: string;
+	message: string;
+}
+
+const ALERTS: Alert[] = [
+	{
+		type: 'danger',
+		message: 'Ocorreu algum erro ao conectar com o backend',
+  }
+];
 
 @Component({
   selector: 'app-status-page',
+  standalone: true,
+	imports: [NgFor, NgbAlertModule, NgIf],
   templateUrl: './status-page.component.html',
   styleUrls: ['./status-page.component.scss']
 })
 export class StatusPageComponent {
+  alerts!: Alert[];
+  hasError!: Boolean;
 
   status!: Status;
-  constructor( private readonly statusService: StatusPageService, private readonly router: Router,){}
+  constructor( private readonly statusService: StatusPageService, private readonly router: Router){
+    this.reset();
+  }
 
 
   ngOnInit(): void {
@@ -22,7 +42,9 @@ export class StatusPageComponent {
     ])
       .pipe(
         catchError((err) => {
-          void this.router.navigate(["/"]);
+          this.hasError = true;
+          this.alerts = Array.from(ALERTS);
+          //void this.router.navigate(["/"]);
           return throwError(err);
         })
       )
@@ -30,4 +52,14 @@ export class StatusPageComponent {
         this.status = status;
       });
   }
+
+	close(alert: Alert) {
+		this.alerts.splice(this.alerts.indexOf(alert), 1);
+    void this.router.navigate(["/"]);
+	}
+
+  reset() {
+		this.alerts = Array.from(ALERTS);
+    this.hasError = false;
+	}
 }
